5842f44eef59f99df57eec45487213035826097f
Muhammad Anas <muhammadannas138@gmail.com>
2026-01-05 18:44:29 +0500

    added socket binding and listening

--- ztransfer.c
+++ ztransfer.c
@@ -13,8 +13,7 @@ int main(int argc, char* argv[]){
         WSACleanup();
         exit(2);
     }
-    struct addrinfo info;
-    struct addrinfo* res;
+    struct addrinfo info, *res, *p;
     memset(&info,0,sizeof info);
     info.ai_family = AF_UNSPEC;
     info.ai_socktype = SOCK_STREAM;
@@ -24,5 +23,31 @@ int main(int argc, char* argv[]){
         fprintf(stderr,"gai error: %s\n", gai_strerror(status));
         exit(1);
     }
+    int sockfd;
+    int yes = 1;
+    for(p = res; p != NULL; p = p->ai_next){
+        if ((sockfd = socket(p->ai_family,p->ai_socktype,p->ai_protocol)) == -1){
+            continue;
+        }
+        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,(const char*)&yes,sizeof(int)) == -1){
+            printf("error in sockopt");
+            exit(1);
+        }
+        if (bind(sockfd,p->ai_addr, p->ai_addrlen)== -1){
+            continue;
+        }
+        break;
+    }
     freeaddrinfo(res);
+    if (p == NULL){
+        printf("could not bind");
+        exit(1);
+    }    
+    if (listen(sockfd,1)==-1){
+        printf("could not listen");
+        exit(1);
+    }
+    while(1){
+
+    }
 }
