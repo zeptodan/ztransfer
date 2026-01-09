43602b9303c902d1875eb52769f0f2737c01c2bd
Muhammad Anas <muhammadannas138@gmail.com>
2026-01-09 20:34:34 +0500

    refactor

--- ztransfer.c
+++ ztransfer.c
@@ -5,8 +5,18 @@
 
 #define MAX_BUF 100
 #define PORT 4000
-
-void windowStartup(){
+#define STR(x) #x
+#define XSTR(x) STR(x)
+#define PORT_STRING XSTR(PORT)
+void print_error(char* msg){
+    #ifdef _WIN32
+    printf("%s: %d\n",msg,WSAGetLastError());
+    #else
+    printf("%s",msg);
+    perror("");
+    #endif
+}
+void window_startup(){
     WSADATA wsadata;
     if (WSAStartup(MAKEWORD(2,2),&wsadata) !=0){
         fprintf(stderr, "WSAStartup failed\n");
@@ -18,42 +28,33 @@ void windowStartup(){
         exit(2);
     }
 }
-void* get_addr(struct sockaddr* their_addr){
-    if (their_addr->sa_family == AF_INET){
-        return &(((struct sockaddr_in*)their_addr)->sin_addr);
-    }
-    else{
-        return &(((struct sockaddr_in6*)their_addr)->sin6_addr);
-    }
-}
 int get_broadcast_socket(){
     int sockfd, broadcast = 1;
     if ((sockfd = socket(AF_INET,SOCK_DGRAM,0))==-1){
-        perror("socket");
+        print_error("socket");
         exit(1);
     }
     if(setsockopt(sockfd,SOL_SOCKET,SO_BROADCAST,(char *)&broadcast,sizeof broadcast)==-1){
-        perror("socket options(broadcast)");
+        print_error("socket options(broadcast)");
         exit(1);
     }
     return sockfd;
 }
-int broadcast(int sockfd,struct sockaddr_in* broadcast_addr){
-    if (sendto(sockfd,"yo homeboy",10,0,(struct sockaddr*)broadcast_addr, sizeof(struct sockaddr_in))==-1){
-        perror("sendto");
-        printf("windows error: %d\n",WSAGetLastError());
+int send_udp_packet(int sockfd,struct sockaddr_in* broadcast_addr,char* msg,int size){
+    if (sendto(sockfd,msg,size,0,(struct sockaddr*)broadcast_addr, sizeof(struct sockaddr_in))==-1){
+        print_error("sendto");
         exit(1);
     }
     return 0;
 }
-int get_broadcast_listener(char* port){
+int get_udp_listener(){
     struct addrinfo info, *res, *p;
     memset(&info,0,sizeof info);
     info.ai_family = AF_INET;
     info.ai_socktype = SOCK_DGRAM;
     info.ai_flags = AI_PASSIVE;
     int status;
-    if ((status = getaddrinfo(NULL,port,&info,&res) !=0)){
+    if ((status = getaddrinfo(NULL,PORT_STRING,&info,&res) !=0)){
         fprintf(stderr,"gai error: %s\n", gai_strerror(status));
         exit(1);
     }
@@ -76,14 +77,14 @@ int get_broadcast_listener(char* port){
     freeaddrinfo(res);
     return sockfd;
 }
-int bindtoSocket(char* port){
+int get_tcp_listener(){
     struct addrinfo info, *res, *p;
     memset(&info,0,sizeof info);
     info.ai_family = AF_INET;
     info.ai_socktype = SOCK_STREAM;
     info.ai_flags = AI_PASSIVE;
     int status;
-    if ((status = getaddrinfo(NULL,port,&info,&res)) !=0){
+    if ((status = getaddrinfo(NULL,PORT_STRING,&info,&res)) !=0){
         fprintf(stderr,"gai error: %s\n", gai_strerror(status));
         exit(1);
     }
@@ -112,67 +113,39 @@ int bindtoSocket(char* port){
     freeaddrinfo(res);
     return sockfd;
 }
-int connecttoSocket(char* port,char s[], int size){
-    struct addrinfo info, *res, *p;
-    memset(&info,0,sizeof info);
-    info.ai_family = AF_UNSPEC;
-    info.ai_socktype = SOCK_STREAM;
-    int status;
-    if ((status = getaddrinfo("localhost",port,&info,&res) !=0)){
-        fprintf(stderr,"gai error: %s\n", gai_strerror(status));
+int get_tcp_socket(struct sockaddr_storage* their_addr){
+    int tcp_fd;
+    struct sockaddr_in connect_addr;
+    memset(&connect_addr,0,sizeof connect_addr);
+    connect_addr.sin_family = ((struct sockaddr_in*)&their_addr)->sin_family;
+    connect_addr.sin_addr = ((struct sockaddr_in*)&their_addr)->sin_addr;    
+    connect_addr.sin_port = htons(PORT);
+    if ((tcp_fd = socket(AF_INET,SOCK_STREAM,0)) == -1){
+        print_error("socket");
         exit(1);
     }
-    int sockfd;
-    for(p = res; p != NULL; p = p->ai_next){
-        if ((sockfd = socket(p->ai_family,p->ai_socktype,p->ai_protocol)) == -1){
-            continue;
-        }
-        inet_ntop(p->ai_family,get_addr((struct sockaddr*)p->ai_addr),s,size);
-        printf("attempting to connect to: %s\n", s);
-        if (connect(sockfd,p->ai_addr,p->ai_addrlen) == -1){
-            closesocket(sockfd);
-            perror("error connecting");
-            printf("windows error: %d\n",WSAGetLastError());
-            continue;
-        }
-        break;
-    }
-    if (p == NULL){
-        printf("could not bind");
+    if (connect(tcp_fd, (struct sockaddr*)&connect_addr,sizeof connect_addr) == -1){
+        print_error("connect");
         exit(1);
     }
-    inet_ntop(p->ai_family,get_addr((struct sockaddr*)p->ai_addr),s,size);
-    printf("connected to: %s\n", s);
-    freeaddrinfo(res);
-    return sockfd;
+    return tcp_fd;
 }
 int discovery(){
     fd_set my_set;
-    int tcp_fd = bindtoSocket("4000");
-
-    struct sockaddr_in addr;
-    int addrlen = sizeof(addr);
-    printf("sockname: %i\n", getsockname(tcp_fd, (struct sockaddr*)&addr, &addrlen));
-    if (getsockname(tcp_fd, (struct sockaddr*)&addr, &addrlen) == SOCKET_ERROR) {
-        printf("getsockname failed: %d\n", WSAGetLastError());
-    } else {
-        char ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &addr.sin_addr, ip, sizeof(ip));
-        printf("Socket is bound to IP: %s, port: %d\n", ip, ntohs(addr.sin_port));
-    }
+    int tcp_fd = get_tcp_listener();
     int udp_fd = get_broadcast_socket();
     struct sockaddr_storage their_addr;
-    int size = sizeof their_addr;
     struct sockaddr_in broadcast_addr;
+    int size = sizeof their_addr;
     broadcast_addr.sin_family = AF_INET;
     broadcast_addr.sin_port = htons(PORT);
+    struct timeval time;
+    int new_fd;
     inet_pton(AF_INET,"192.168.1.255",&(broadcast_addr.sin_addr));
     if (listen(tcp_fd,1)==-1){
         printf("could not listen");
         exit(1);
     }
-    struct timeval time;
-    int new_fd;
     while(1){
         FD_ZERO(&my_set);
         FD_SET(tcp_fd,&my_set);
@@ -180,12 +153,12 @@ int discovery(){
         time.tv_usec = 0;
         int timer = select(tcp_fd + 1,&my_set,NULL, NULL,&time);
         if (timer == 0){
-            broadcast(udp_fd,&broadcast_addr);
+            send_udp_packet(udp_fd,&broadcast_addr,"yo homeboy",10);
             continue;
         }
         else if(FD_ISSET(tcp_fd,&my_set)){
             if ((new_fd = accept(tcp_fd,(struct sockaddr*)&their_addr,& size)) == -1){
-                perror("accept");
+                print_error("accept");
                 continue;
             }
             break;
@@ -209,7 +182,7 @@ int listen_to_discovery(){
     fd_set my_set;
     char buf[MAX_BUF];
     int bytes;
-    int udp_fd = get_broadcast_listener("4000");
+    int udp_fd = get_udp_listener();
     int tcp_fd;
     struct sockaddr_storage their_addr;
     int size = sizeof their_addr;
@@ -225,7 +198,7 @@ int listen_to_discovery(){
         }
         else if(FD_ISSET(udp_fd,&my_set)){
             if ((bytes = recvfrom(udp_fd,buf,MAX_BUF,0,(struct sockaddr*)&their_addr,&size)) == -1){
-                perror("recv");
+                print_error("recv");
                 continue;
             }
             buf[bytes] = '\0';
@@ -233,90 +206,23 @@ int listen_to_discovery(){
             break;
         }
         else{
-            perror("unknown");
-            printf("windows error: %d",WSAGetLastError());
+            print_error("unknown");
             break;
         }
     }
     closesocket(udp_fd);
-    struct sockaddr_in connect_addr;
-    memset(&connect_addr,0,sizeof connect_addr);
-    connect_addr.sin_family = ((struct sockaddr_in*)&their_addr)->sin_family;
-    connect_addr.sin_addr.S_un.S_addr = INADDR_ANY;
-    connect_addr.sin_port = htons(PORT);
-    if ((tcp_fd = socket(AF_INET,SOCK_STREAM,0)) == -1){
-        perror("socket");
-        printf("windows error: %d",WSAGetLastError());
-        exit(1);
+    int tcp_fd = get_tcp_socket(&their_addr);
+    if (send(tcp_fd,"hell naw",8,0) == -1){
+        print_error("send");
     }
-    // if (bind(tcp_fd,(struct sockaddr*)&connect_addr,sizeof connect_addr) == -1){
-    //     perror("bind");
-    //     printf("windows error: %d",WSAGetLastError());
-    //     exit(1);
-    // }
-    connect_addr.sin_addr = ((struct sockaddr_in*)&their_addr)->sin_addr;
-
-    
-    char ip[INET_ADDRSTRLEN];
-    inet_ntop(AF_INET, &connect_addr.sin_addr, ip, sizeof(ip));
-    printf("Socket bound to IP: %s, port: %d\n", ip, ntohs(connect_addr.sin_port));
-    
-    if (connect(tcp_fd, (struct sockaddr*)&connect_addr,sizeof connect_addr) == -1){
-        perror("connect");
-        printf("windows error: %d",WSAGetLastError());
-        exit(1);
-    }
-    send(tcp_fd,"hell naw",8,0);
     return 0;
 }
 int main(int argc, char* argv[]){
     if (argc != 2){
         printf("expected argument ./ztransfer [send | receive]\n");
     }
-    windowStartup();  
-    int sockfd;
-    char* port = "4000";
-    socklen_t size;
-    char s[INET6_ADDRSTRLEN];
-    if (strcmp(argv[1],"receive") == 0){
-        char buf[MAX_BUF];
-        int totbytes;
-        sockfd = connecttoSocket(port,s,sizeof s);
-        if((totbytes = recv(sockfd,buf,MAX_BUF -1,0)) == -1){
-            perror("receive error");
-            printf("windows error: %d",WSAGetLastError());
-            exit(1);
-        }
-        if (totbytes == 0){
-            printf("connection closed by peer\n");
-        }
-        buf[totbytes] = '\0';
-        printf("bytes received: %s\n", buf);
-        closesocket(sockfd);
-    }
-    else if (strcmp(argv[1],"send") == 0){
-        struct sockaddr_storage their_addr;
-        size = sizeof their_addr;
-        sockfd = bindtoSocket(port);
-        if (listen(sockfd,2)==-1){
-            printf("could not listen");
-            exit(1);
-        }
-        int new_fd;
-        while(1){
-            new_fd = accept(sockfd, (struct sockaddr*)&their_addr,&size);
-            if (new_fd == -1){
-                continue;
-            }
-            inet_ntop(their_addr.ss_family,get_addr((struct sockaddr*)&their_addr),s,sizeof s);
-            printf("server got connection from: %s\n", s);
-            if(send(new_fd,"sup homie",9,0) == -1){
-                perror("error sending message");
-            }
-            closesocket(new_fd);
-        }
-    }
-    else if (strcmp(argv[1],"broadcast") == 0){
+    window_startup();
+    if (strcmp(argv[1],"broadcast") == 0){
         discovery();
     }
     else if (strcmp(argv[1],"listen") == 0){
