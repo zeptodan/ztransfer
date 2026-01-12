ce1fff40467bf5b1f1cd1c081cfb696b9c30d7eb
Muhammad Anas <muhammadannas138@gmail.com>
2026-01-12 19:56:02 +0500

    added broadcast list

--- ztransfer.c
+++ ztransfer.c
@@ -5,19 +5,71 @@
 #include<stdbool.h>
 #include<WinSock2.h>
 #include<WS2tcpip.h>
-#define MAX_BUF 100
+#define MAX_BUF 256
 #define PORT 4000
-#define MAX_NAME 256
+#define MAX_NAME MAX_BUF
 #define MAX_PEERS 8
+#define TIMEOUT 3
 #define STR(x) #x
 #define XSTR(x) STR(x)
 #define PORT_STRING XSTR(PORT)
-typedef struct {
-    struct addr_storage* addr;
+typedef struct Broadcast{
+    struct sockaddr_storage addr;
     char* name;
     time_t time;
+    bool (*is_old)(struct Broadcast*);
 }Broadcast;
-Broadcast broadcasts[MAX_PEERS];
+typedef struct BroadcastList{
+    Broadcast broadcasts[MAX_PEERS];
+    int size;
+    int (*add)(struct BroadcastList*,struct sockaddr_storage, char*);
+    int (*clean)(struct BroadcastList*);
+    int (*list_free)(struct BroadcastList*);
+} BroadcastList;
+bool is_old(Broadcast* self){
+    return time(NULL) - self->time > TIMEOUT;
+}
+bool check_broadcast_same(struct sockaddr_storage* addr1,struct sockaddr_storage* addr2){
+    return memcmp((&((struct sockaddr_in*)addr1)->sin_addr.S_un.S_addr),&(((struct sockaddr_in*)addr2)->sin_addr.S_un.S_addr), sizeof(((struct sockaddr_in*)addr2)->sin_addr.S_un.S_addr));
+}
+int add(BroadcastList* self,struct sockaddr_storage addr, char* name){
+    for (int i = 0;i < self->size;i++){
+        if(check_broadcast_same(&self->broadcasts[i].addr,&addr) == 0){
+            self->broadcasts[i].time = time(NULL);
+            return 0;
+        }
+    }
+    if (self->size < MAX_PEERS){
+        Broadcast broadcast = {addr,name,time(NULL),is_old};
+        self->broadcasts[self->size++] = broadcast;
+        return 0;
+    }
+    return -1;
+}
+int clean(BroadcastList* self){
+    for (int i = 0;i < self->size;i++){
+        if (self->broadcasts[i].is_old(&self->broadcasts[i])){
+            memmove(&self->broadcasts[i],&self->broadcasts[i+1],self->size - i - 1);
+            self->size--;
+        }
+    }
+    return 0;
+}
+int list_free(BroadcastList* self){
+    for (int i = 0;i < self->size;i++){
+        free(self->broadcasts[i].name);
+    }
+    free(self);
+    return 0;
+}
+BroadcastList* list_constructor(){
+    BroadcastList* list = malloc(sizeof(BroadcastList));
+    list->size = 0;
+    list->clean = clean;
+    list->list_free = list_free;
+    list->add = add;
+    return list;
+}
 void print_error(char* msg){
     #ifdef _WIN32
     printf("%s: %d\n",msg,WSAGetLastError());
@@ -161,23 +213,25 @@ int discovery(){
         printf("could not listen");
         exit(1);
     }
+    HANDLE h = GetStdHandle(STD_INPUT_HANDLE);
     while(1){
         FD_ZERO(&my_set);
         FD_SET(tcp_fd,&my_set);
-        time.tv_sec = 1;
-        time.tv_usec = 0;
-        int timer = select(tcp_fd + 1,&my_set,NULL, NULL,&time);
-        if (timer == 0){
-            send_udp_packet(udp_fd,&broadcast_addr,hostname,name_len);
-            continue;
-        }
-        else if(FD_ISSET(tcp_fd,&my_set)){
+        time.tv_sec = 0;
+        time.tv_usec = 10000;
+        send_udp_packet(udp_fd,&broadcast_addr,hostname,name_len);
+        select(tcp_fd + 1,&my_set,NULL, NULL,&time);
+        if(FD_ISSET(tcp_fd,&my_set)){
             if ((new_fd = accept(tcp_fd,(struct sockaddr*)&their_addr,& size)) == -1){
                 print_error("accept");
                 continue;
             }
             break;
         }
+        // if(WaitForSingleObject(h,990)== WAIT_OBJECT_0){
+        //     char c = getchar();
+        //     printf("i read that: %c\n", c);
+        // }
     }
     closesocket(udp_fd);
     closesocket(tcp_fd);
@@ -192,30 +246,39 @@ int listen_to_discovery(){
     struct sockaddr_storage their_addr;
     int size = sizeof their_addr;
     struct timeval time;
+    HANDLE h = GetStdHandle(STD_INPUT_HANDLE);
+    BroadcastList* list = list_constructor();
     while(1){
         FD_ZERO(&my_set);
         FD_SET(udp_fd,&my_set);
-        time.tv_sec = 1;
-        time.tv_usec = 0;
-        int timer = select(udp_fd+1,&my_set,NULL,NULL,&time);
-        if (timer == 0){
-            continue;
-        }
-        else if(FD_ISSET(udp_fd,&my_set)){
+        time.tv_sec = 0;
+        time.tv_usec = 10000;
+        select(udp_fd+1,&my_set,NULL,NULL,&time);
+        if(FD_ISSET(udp_fd,&my_set)){
             if ((bytes = recvfrom(udp_fd,buf,MAX_BUF,0,(struct sockaddr*)&their_addr,&size)) == -1){
                 print_error("recv");
                 continue;
             }
             buf[bytes] = '\0';
+            char* name = malloc(bytes+1);
+            strcpy(name,buf);
+            list->add(list,their_addr,name);
             printf("bytes recv: %s\n", buf);
+            for(int i = 0; i < list->size;i++){
+                printf("name: %s\n",list->broadcasts[i].name);
+            }
             break;
         }
-        else{
-            print_error("unknown");
-            break;
-        }
+        list->clean(list);
+        // if(WaitForSingleObject(h,990) == WAIT_OBJECT_0){
+        //     char c = getchar();
+        //     printf("i read that: %c\n", c);
+        //     if(c == 'q')
+        //         break;
+        // }
     }
     closesocket(udp_fd);
+    list->list_free(list);
     tcp_fd = get_tcp_socket(&their_addr);
     return tcp_fd;
 }
